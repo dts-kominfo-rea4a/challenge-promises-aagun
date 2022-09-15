1a9a472edbb6a4960717b33fe82da0ac326c6462
agun <agunfahminurhakiki@gmail.com>
2022-09-16 00:22:11 +0700

    Count film by audience emotions

--- promise.js
+++ promise.js
@@ -1,7 +1,32 @@
-const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
+const { promiseTheaterIXX, promiseTheaterVGC } = require('./external.js');
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const subtract = (a, b) => a + b;
+
+const increment = (currVal) => subtract(currVal, 1);
+
+const hasValue = (obj, prop, emosi) => {
+  return obj[prop] === emosi.toLowerCase();
+};
+
+const checkValue = (obj, prop, emosi) => {
+  return obj.hasOwnProperty(prop) && obj[prop] && hasValue(obj, prop, emosi);
+};
+
+const promiseOutput = async (emosi) => {
+  return await promiseTheaterIXX().then(async (ixx) => {
+    const vgc = await promiseTheaterVGC();
+    const data = [...vgc, ...ixx];
+    const prop = 'hasil';
+    let result = 0;
+
+    data.forEach((obj) => {
+      if (checkValue(obj, prop, emosi)) result = increment(result);
+    });
+
+    return result;
+  });
+};
 
 module.exports = {
   promiseOutput,
